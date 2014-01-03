e55636bb584bf0587acd1f7ad99e59e018ecc5b9
Rodney Persky <rodney.persky@gmail.com>
2014-01-03 20:15:30 +1000

    Added Tkinter interface, rearranged some functions, added ctrl-c hook to unmute, moved blocklist to its own file so you can exchange them with a pastie

---
+++ blockify.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+blockify:
+    Mute Spotify tracks (only works on the wine version)
+    requires wmctrl to be installed.
+    
+    When you find a track you want to mute, you need to add it to
+    track_list. Find out the name with wmctrl -l when the track is playing.
+    you only need the part after "Spotify - " and you can shorten if you
+    want, e.g. Spotify - Bloodhound Gang – Along Comes Mary becomes
+    Bloodhound, which would mute all tracks that start with Bloodhound
+    
+    After adding a new entry you need to restart blockify manually or with:
+    pkill -USR1 -f blockify
+    
+    cheers
+"""
+
+import subprocess, time, sys, os, signal
+spotify = "Spotify -"
+track_list_filename = "track_list.txt"
+
+# Method for loading track_list from a file
+def load_track_list():
+    track_list_file = open(track_list_filename, "r")
+    track_list = track_list_file.read()
+    track_list_file.close()
+    track_list = track_list.split("\n")
+    track_list = [spotify + " " + s for s in track_list]
+    return track_list
+
+# Method to add to the track_list file
+def modify_track_list(artist_album):
+    track_list_file = open(track_list_filename, "r")
+    current_track_list = track_list_file.read()
+    track_list_file.close()
+    
+    track_list = current_track_list + "\n" + artist_album
+    track_list_file = open(track_list_filename, "w")
+    track_list_file.write(track_list)
+    track_list_file.close()
+    
+    return load_track_list()
+
+# When the program starts, the track_list is read
+track_list = load_track_list()
+
+
+def mute():
+    subprocess.Popen(['amixer', '-q', 'set', 'Master', 'mute'])
+    global is_muted
+    is_muted = True
+
+def unmute():
+    subprocess.Popen(['amixer', '-q', 'set', 'Master', 'unmute'])
+    global is_muted
+    is_muted = False
+  
+def restart():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+def signal_handler(signum, frame):
+    if signum == 2:
+        print 'Exiting'
+        unmute()
+        sys.exit(0)
+    else:
+        print signum, 'Restarting'
+        restart()
+
+signal.signal(signal.SIGUSR1, signal_handler)
+signal.signal(signal.SIGINT, signal_handler)
+
+def get_windows():
+    pipe = get_windows_pipe()
+    separator = ""
+    return separator.join(pipe_readlines(pipe))
+  
+def get_windows_pipe():
+    pipe = subprocess.Popen(['wmctrl', '-l'], stdout=subprocess.PIPE).stdout
+    return pipe
+
+def pipe_readlines(pipe):
+    try:
+        return pipe.readlines()
+    except:
+        return ""
+    
+def check_tracklist(windows):
+    found = False
+    
+    for track in track_list:
+        if (windows.find(track.strip('\n')) >= 0):
+            found = True
+            break
+      
+    if found:
+        if (not is_muted):
+            mute()
+            print 'Muting'
+    elif is_muted:
+        unmute()
+        
+    return found
+
+def get_playing(windows, artist_album=""):
+    window_list = windows.split("\n")
+    
+    try:       
+        for item in window_list:
+            if spotify in item:
+                window_title = item[item.find(spotify):].split("-")
+                artist_album = window_title[1].strip()
+    except:
+        pass
+    
+    return artist_album
+
+def main():
+    global is_muted    
+    is_muted = False
+    unmute()
+  
+    while(True):
+        windows = get_windows()
+        check_tracklist(windows)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    print "Starting Blockify"
+    main()
---
+++ blockifytk.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+blockify-tk:
+    This is a TKinter interface which looks like the windows version
+    of Blockify.
+"""
+
+import sys, os, inspect
+from Tkinter import *
+import tkMessageBox
+
+
+import blockify as blockify
+blockify.unmute()
+
+# Create TK App
+app = Tk()
+app.title("Blockify")
+
+# Song Title Label
+string_artist_album = StringVar()
+string_artist_album.set("Hey!? How are you doing?")
+label_artist_album = Label(app, textvariable=string_artist_album, font=("sans", 14))
+label_artist_album.grid(row=1, column=1, columnspan=2)
+
+# Mute track_list check box
+bool_mute_adverts = IntVar()
+bool_mute_adverts.set(1)
+check_mute_adverts = Checkbutton(app, text = "Mute Adverts",\
+                     variable=bool_mute_adverts, onvalue=1, offvalue=0)
+check_mute_adverts.grid(row=2, column=2)
+
+# Block currently playing
+button_add_track = Button(app, text="Block This Track")
+button_add_track.grid(row=2, column=1)
+
+
+def toggle_block_current():
+    window_list = blockify.get_windows()
+    artist_album = blockify.get_playing(window_list)
+        
+    action = tkMessageBox.askquestion("Confirm",
+            "Add {} to your block file?".format(artist_album))
+    
+    if action == "yes":
+        blockify.track_list = blockify.modify_track_list(artist_album)
+        
+# All functions
+def update_gui():
+    window_list = blockify.get_windows()
+    artist_album = blockify.get_playing(window_list)
+    
+    string_artist_album.set(artist_album)
+
+    if artist_album is not "":
+        if bool_mute_adverts.get():
+            if blockify.check_tracklist(window_list):
+                button_add_track.config(text="    Blocked     ")
+            else:
+                button_add_track.config(text="Block This Track")
+        else:
+            blockify.unmute()
+    else:
+        blockify.unmute()
+    
+    app.after(1000, update_gui) #Get title every 2 seconds
+
+# Callbacks
+button_add_track.config(command=toggle_block_current)
+app.after(10, update_gui)
+
+# Start App
+app.mainloop()
+
+
+# Unmute on exit
+blockify.unmute()
