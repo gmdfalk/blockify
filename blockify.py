896d6e4ed869cbaea0cb9c436355fbd17636050e
Jesse Maes <fibonacci713@gmail.com>
2014-02-18 23:26:15 -0700

    Made pylint quit it's bitchin'

--- blockify.py
+++ blockify.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
 
+import re
+import time
+import subprocess
 from spotify import Spotify
 
-import time
+def toggle_mute(force=False):
+    '''Finds spotify's audio sink and toggles it between muted and not muted'''
+
+    pacmd_out = subprocess.check_output(["pacmd", "list-sink-inputs"])
+    pidof_out = subprocess.check_output(["pidof", "spotify"])
+
+    pattern = re.compile(r'(?: index|muted|application\.process\.id).*?(\w+)')
+    pids = pidof_out.decode('utf-8').strip().split(' ')
+    output = pacmd_out.decode('utf-8')
+
+    # Every third element is a key, the value is the preceding two
+    # elements in the form of a tuple - {pid : (index, muted)}
+    info = pattern.findall(output)
+    idxd = {info[3*n+2] : (info[3*n], info[3*n+1])
+            for n in range(0, len(info) // 3)}
+
+    pid = [k for k in idxd.keys() if k in pids][0]
+    index, muted = idxd[pid]
+
+    if muted == 'no' or force:
+        subprocess.call(["pacmd", "set-sink-input-mute", index, '1'])
+    else:
+        subprocess.call(["pacmd", "set-sink-input-mute", index, '0'])
+
 
 def blockify(spotify):
+    '''Checks if an ad is playing. If so mutes spotify until a new song
+    comes on.'''
     spotify.prev()
 
     now = int(time.time())
@@ -13,39 +41,39 @@ def blockify(spotify):
     end_time = now + length
 
     while spotify.is_running():
+        now = int(time.time())
         cur_title = spotify.get_song_title()
-        
+
         if cur_title != title:
-            print("Song Changed")
-            print("Song ends in ", spotify.get_song_length(), " seconds")
+            print("CHANGED: Song ends in", spotify.get_song_length(), "seconds")
             length = spotify.get_song_length()
             end_time = now + length
             title = cur_title
 
         status = str(spotify.get_property('PlaybackStatus'))
-        
-        while status == 'Paused':
-            now = int(time.time())
-            status = str(spotify.get_property('PlaybackStatus'))
+
+        if status == 'Paused':
             print("PAUSED: Song ends in", end_time - now, "seconds")
-            end_time += 1
-            time.sleep(1)
-        
-        now = int(time.time())
-        
+            while status == 'Paused':
+                time.sleep(1)
+                end_time += 1
+                status = str(spotify.get_property('PlaybackStatus'))
+            print("PLAYING")
+
         if now > end_time and cur_title == title:
-            print("Muting")
-            spotify.toggle_mute()
+            print("MUTING")
+            toggle_mute()
             while cur_title == title:
                 cur_title = spotify.get_song_title()
                 time.sleep(0.5)
-            print("Unuting")
-            spotify.toggle_mute()
-                
+            print("UNUTING")
+            toggle_mute()
+
         time.sleep(1)
-            
+
 
 def main():
+    '''Blockifies a Spotify!'''
     spotify = Spotify()
     blockify(spotify)
 
--- spotify.py
+++ spotify.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
 
-import subprocess
 import dbus
-import re
 
-class Spotify():
+class Spotify(object):
+    '''Wrapper for Spotify's DBus interface'''
     prop_path = 'org.freedesktop.DBus.Properties'
     player_path = 'org.mpris.MediaPlayer2.Player'
     spotify_path = 'org.mpris.MediaPlayer2.spotify'
     running = False
-    
+
     session_bus = None
     proxy = None
     properties = None
     player = None
 
     def __init__(self):
-        self.session_bus = dbus.SessionBus() 
+        self.session_bus = dbus.SessionBus()
         try:
             self.proxy = self.session_bus.get_object(self.spotify_path,
                                                      '/org/mpris/MediaPlayer2')
         except:
             self.proxy = None
             print("Is Spotify not runnung?")
-            
-        if self.proxy: 
+
+        if self.proxy:
             self.running = True
             self.properties = dbus.Interface(self.proxy, self.prop_path)
             self.player = dbus.Interface(self.proxy, self.player_path)
 
-            
+
     def is_running(self):
+        '''TODO: Make this not redundant'''
         return self.running
 
-    
+
     def get_property(self, key):
-        if self.proxy:
+        '''Gets the value from any available property'''
+        if self.properties:
             return self.properties.Get(self.player_path, key)
 
 
     def toggle_pause(self):
+        '''Calls PlayPause method'''
         if self.player:
             can_pause = self.get_property('CanPause')
             can_play = self.get_property('CanPlay')
@@ -48,8 +50,9 @@ class Spotify():
             else:
                 print("Cannot Play/Pause")
 
-            
+
     def next(self):
+        '''Tries to skip to next song'''
         if self.player:
             can_next = self.get_property('CanGoNext')
             if can_next:
@@ -57,8 +60,9 @@ class Spotify():
             else:
                 print("Cannot Go Next")
 
-            
+
     def prev(self):
+        '''Tries to go back to last song'''
         if self.player:
             can_prev = self.get_property('CanGoPrevious')
             if can_prev:
@@ -68,6 +72,7 @@ class Spotify():
 
 
     def seek(self, seconds):
+        '''Calls (nonworking?) Seek method'''
         if self.player:
             can_seek = self.get_property('CanSeek')
             if can_seek:
@@ -77,68 +82,51 @@ class Spotify():
 
 
     def get_song_length(self):
+        '''Gets the length of current song from metadata (in seconds)'''
         metadata = self.get_property('Metadata')
         if metadata:
             return int(metadata['mpris:length'] / 1000000)
 
-            
+
     def get_song_title(self):
+        '''Gets title of current song from metadata'''
         metadata = self.get_property('Metadata')
         if metadata:
             return str(metadata['xesam:title'])
 
-            
+
     def get_song_artist(self):
+        '''Gets the artist of current song from metadata'''
         metadata = self.get_property('Metadata')
         if metadata:
             return str(metadata['xesam:artist'][0])
 
-            
-    def toggle_mute(self, force=False):
-        pacmd_out = subprocess.check_output(["pacmd", "list-sink-inputs"])
-        pidof_out = subprocess.check_output(["pidof", "spotify"])
-        
-        pattern = re.compile(r'(?: index|muted|application\.process\.id).*?(\w+)')
-        output = pacmd_out.decode('utf-8')
-        
-        # Every third element is a key, the value is the preceding two
-        # elements put into a tuple - {pid : (index, muted)}
-        l = pattern.findall(output)
-        d = {l[3*n+2] : (l[3*n], l[3*n+1]) for n in range(0, len(l) // 3)}
-        
-        pids = pidof_out.decode('utf-8').strip().split(' ')
-        pid = [k for k in d.keys() if k in pids][0]
-        index, muted = d[pid]
-        
-        if muted == 'no' or force:
-            subprocess.call(["pacmd", "set-sink-input-mute", index, '1'])
-        else:
-            subprocess.call(["pacmd", "set-sink-input-mute", index, '0'])
-        
-            
+
     def print_info(self):
+        '''Prints all the information I can get from the dbus interface. Some
+        of it is incorrect, but it's what I've got!'''
         if self.properties:
             interfaces = self.properties.GetAll(self.player_path)
             metadata = self.get_property('Metadata')
-            
+
             i_keys = list(map(str, interfaces.keys()))
             i_keys.remove('Metadata')
             i_keys.sort()
-            
+
             for i in i_keys:
                 if len(i) < 7:
                     print(i, "\t\t= ", self.get_property(i))
                 else:
                     print(i, "\t= ", self.get_property(i))
-                    
+
             print("")
-                    
+
             d_keys = list(metadata.keys())
             d_keys.sort()
 
             for k in d_keys:
                 d = k.split(':')[1]
-                
+
                 if d == 'artist':
                     print(d, "\t\t= ", metadata[k][0])
                 # elif d == 'length':
