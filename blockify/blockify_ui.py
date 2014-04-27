3a0c3b5444f2cb6a8d4f9883ef62339a21144fe2
max demian <mikar@gmx.de>
2014-04-27 21:08:16 +0200

    n/a

--- blockify/blockify_ui.py
+++ blockify/blockify_ui.py
@@ -14,6 +14,65 @@ class Notepad(gtk.Window):
         self.set_wmclass("blocklist", "Blocklist")
         self.show()
 
+class BasicTreeViewExample(object):
+
+    # close the window and quit
+    def delete_event(self, widget, event, data=None):
+        gtk.main_quit()
+        return False
+
+    def __init__(self):
+        # Create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        self.window.set_title("Basic TreeView Example")
+
+        self.window.set_size_request(200, 200)
+
+        self.window.connect("delete_event", self.delete_event)
+
+        # create a TreeStore with one string column to use as the model
+        self.treestore = gtk.TreeStore(str)
+
+        # we'll add some data now - 4 rows with 3 child rows each
+        for parent in range(4):
+            piter = self.treestore.append(None, ['parent %i' % parent])
+            for child in range(3):
+                self.treestore.append(piter, ['child %i of parent %i' %
+                                              (child, parent)])
+
+        # create the TreeView using treestore
+        self.treeview = gtk.TreeView(self.treestore)
+
+        # create the TreeViewColumn to display the data
+        self.tvcolumn = gtk.TreeViewColumn('Column 0')
+
+        # add tvcolumn to treeview
+        self.treeview.append_column(self.tvcolumn)
+
+        # create a CellRendererText to render the data
+        self.cell = gtk.CellRendererText()
+
+        # add the cell to the tvcolumn and allow it to expand
+        self.tvcolumn.pack_start(self.cell, True)
+
+        # set the cell "text" attribute to column 0 - retrieve text
+        # from that column in treestore
+        self.tvcolumn.add_attribute(self.cell, 'text', 0)
+
+        # make it searchable
+        self.treeview.set_search_column(0)
+
+        # Allow sorting on the column
+        self.tvcolumn.set_sort_column_id(0)
+
+        # Allow drag and drop reordering of rows
+        self.treeview.set_reorderable(True)
+
+        self.window.add(self.treeview)
+
+        self.window.show_all()
+
 
 class BlockifyUI(gtk.Window):
 
@@ -69,7 +128,7 @@ class BlockifyUI(gtk.Window):
     def on_openlist(self, widget):
         if widget.get_active():
             widget.set_label("Close Blocklist")
-            self.n = Notepad()
+            self.n = BasicTreeViewExample()
         else:
             widget.set_label("Open Blocklist")
             self.n.destroy()
